0764d2da38bf56ec0c474b51c9be31ab43334364
soncu <zigana_m16@windowslive.com>
2024-10-02 14:08:35 +0300

    Update Jump Game - Leetcode 55.cpp
    better start from the previous element of the last one

--- Jump Game - Leetcode 55/Jump Game - Leetcode 55.cpp
+++ Jump Game - Leetcode 55/Jump Game - Leetcode 55.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int target = n - 1;
 
-        for (int i = n - 1; i >= 0; --i) {
+        for (int i = n - 2; i >= 0; --i) {
             if (i + nums[i] >= target) {
                 target = i;
             }
